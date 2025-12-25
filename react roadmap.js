052c7e87448d0f53eb9a774e7c709aa5ae678554
Malik Yasir <my4871767@gmail.com>
2025-12-25 18:26:44 +0500

    first commit

---
+++ react roadmap.js
@@ -0,0 +1,8 @@
+// React Roadmap 
+// why to learn react 
+// hype job trend job build ui 
+// makes easy to manage and bulid comples front end 
+// why should i learn react ?
+// after mastering js most project does not need reacgt in initial phase
+// js manages our state and dom mananges our ui(user interface)
+// for syncing js and dom they establish react 
