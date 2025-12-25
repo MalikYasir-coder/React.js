c5767eb87faafad0349a5034255cd6b0f28a6959
Malik Yasir <my4871767@gmail.com>
2025-12-25 21:37:09 +0500

    first commit

--- react roadmap.js
+++ react roadmap.js
@@ -6,3 +6,21 @@
 // after mastering js most project does not need reacgt in initial phase
 // js manages our state and dom mananges our ui(user interface)
 // for syncing js and dom they establish react 
+// dont learn react if u dont know about working of js and dom 
+// React  learning process 
+// 1 go in depth 2 by making projects
+// react is a library not a framework
+// topics to learn of react 
+// core of react (state, ui manipulation, jsx)
+// component reusability(multiple usage of buttons)
+// during resuability we can also learn about properties properties{
+// key :value just for example}
+// how to change propagates(hooks/ usestate useeffect)
+// React makes spa (single page application ) which never reloads
+// Additional Adone to React 
+// Additional package react router dom 
+// Router (because react does not have Router)
+// Statemanagement(due to many variables and buttons)
+// javascript libraries(redux,redux toolkit,zustand context api)
+// class base component (legacy code)
+// there is no seo in react 
